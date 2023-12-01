7bf3228f79969b8cf20b80a8a30a89fca6ea6a13
Eduarda Souto <mariaeduardac604@gmail.com>
2023-12-01 15:20:48 -0300

    .

--- src/main/java/br/edu/ifpb/gui/ContatoWindow.java
+++ src/main/java/br/edu/ifpb/gui/ContatoWindow.java
@@ -1,112 +1,322 @@
 package main.java.br.edu.ifpb.gui;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
 
-import main.java.br.edu.ifpb.commands.AdicionarContatoGUICommand;
-import main.java.br.edu.ifpb.commands.CommandExecutor;
-import main.java.br.edu.ifpb.commands.EditarContatoCommand;
 import main.java.br.edu.ifpb.domain.Contato;
 
-import java.awt.*;
-import java.text.SimpleDateFormat;
-
-public class ContatoWindow extends javax.swing.JFrame{
+public class ContatoWindow extends javax.swing.JFrame {
     private ImagePanel imagePanel;
+    private ButtonGroup buttonGroup;
 
-    private final JFrame frame;    
-    private final JTextField txtNome;    
-    private final JTextField txtSobrenome;
-    private final JTextField txtValorDaEntrada;
-    private final JTextField txtRedeSocial;    
-    private final JTextField txtTelefone;
-    private final JTextField txtAniversario;
 
-    public ContatoWindow(MainWindow main, Contato contato){
+
+    public ContatoWindow(MainWindow main, Contato contato) {
         imagePanel = new ImagePanel("Blue wallpaper.png");
         setContentPane(imagePanel);
+        initComponents();
+        setLocationRelativeTo(null);
+    }
 
-        
-        CommandExecutor commandExecutor = new CommandExecutor();
-        
-        frame = new JFrame("Adicionar Contato");
-        JLabel labelNome = new JLabel("Nome:");
-        JLabel labelSobrenome = new JLabel("Sobrenome:");
-        JLabel labelRedeSocial = new JLabel("Rede Social:");
-        JLabel labelValorDeEntrada = new JLabel("Dado específico:");
-        JLabel labelTelefone = new JLabel("Telefone:");        
-        JLabel labelAniversario = new JLabel("Aniversario:");
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
+        jRadioButton3 = new javax.swing.JRadioButton();
 
-        txtNome = new JTextField();        
-        txtSobrenome = new JTextField();
-        txtRedeSocial = new JTextField();
-        txtValorDaEntrada = new JTextField();
-        txtTelefone = new JTextField();
-        txtAniversario = new JTextField();
+        buttonGroup = new ButtonGroup();
+        buttonGroup.add(jRadioButton1);
+        buttonGroup.add(jRadioButton2);
+        buttonGroup.add(jRadioButton3);
 
-        if(contato != null){
-            txtNome.setText(contato.getNome());            
-            txtSobrenome.setText(contato.getSobrenome());
-            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-            txtAniversario.setText(df.format(contato.getAniversario()));
-        }
+        jLabel6 = new javax.swing.JLabel();
+        jTextFieldWhatsApp = new javax.swing.JTextField();
+        jTextFieldEmail = new javax.swing.JTextField();
+        jTextFieldInstagram = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
 
-        JButton btnCriar = new JButton(contato == null ? "Criar" : "Editar");
-        btnCriar.addActionListener(e -> {
-            if(contato == null){
-                commandExecutor.executeCommand(new AdicionarContatoGUICommand(txtNome, txtSobrenome, txtAniversario, txtSobrenome, txtNome, txtValorDaEntrada, txtRedeSocial, txtTelefone, txtAniversario, main));
-            } 
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-            main.update(null);
+        jLabel1.setText("Adicionar Contato");
+
+        jLabel2.setText("Nome");
+
+        jTextField1.setText("");
+        jTextField1.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
         });
 
-        JButton btnCancelar = new JButton("Cancelar");
-        btnCancelar.addActionListener(e -> frame.setVisible(false));
+        jLabel3.setText("Sobrenome");
 
-        JPanel fieldsPanel = new JPanel();
-        fieldsPanel.setLayout(new BoxLayout(fieldsPanel, BoxLayout.Y_AXIS));
-        fieldsPanel.setBorder(new EmptyBorder(10, 10, 0, 10));
-        labelNome.setAlignmentX(Component.LEFT_ALIGNMENT);
-        fieldsPanel.add(labelNome);
-        txtNome.setAlignmentX(Component.LEFT_ALIGNMENT);
-        txtNome.setMaximumSize(new Dimension(400, 30));
-        fieldsPanel.add(txtNome);
+        jLabel4.setText("Telefone");
 
-        labelAniversario.setAlignmentX(Component.LEFT_ALIGNMENT);
-        fieldsPanel.add(labelAniversario);
-        txtAniversario.setAlignmentX(Component.LEFT_ALIGNMENT);
-        txtAniversario.setMaximumSize(new Dimension(400, 30));
-        fieldsPanel.add(txtAniversario);
+        jTextField3.setText("");
+        jTextField3.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
 
-        labelTelefone.setAlignmentX(Component.LEFT_ALIGNMENT);
-        fieldsPanel.add(labelTelefone);
-        txtTelefone.setAlignmentX(Component.LEFT_ALIGNMENT);
-        txtTelefone.setMaximumSize(new Dimension(400, 30));
-        fieldsPanel.add(txtTelefone);
+        jLabel5.setText("Rede Social");
 
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
-        buttonPanel.add(Box.createHorizontalGlue());
-        buttonPanel.add(btnCancelar);
-        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
-        buttonPanel.add(btnCriar);
+        jTextField4.setText("");
+        jTextField4.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextField4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField4ActionPerformed(evt);
+            }
+        });
 
-        frame.add(fieldsPanel, BorderLayout.CENTER);
-        frame.add(buttonPanel, BorderLayout.PAGE_END);
-    }
+        jRadioButton1.setLabel("WhatsApp");
+
+        jRadioButton2.setLabel("Email");
+
+        jRadioButton3.setLabel("Instagram");
+
+        jLabel6.setVisible(false);
+
+        jTextFieldWhatsApp.setVisible(false);
+        jTextFieldWhatsApp.setText("");
+        jTextFieldWhatsApp.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextFieldWhatsApp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldWhatsAppActionPerformed(evt);
+            }
+        });
+
+        jTextFieldEmail.setVisible(false);
+        jTextFieldEmail.setText("");
+        jTextFieldEmail.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextFieldEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldEmailActionPerformed(evt);
+            }
+        });
+
+        jTextFieldInstagram.setVisible(false);
+        jTextFieldInstagram.setText("");
+        jTextFieldInstagram.setPreferredSize(new java.awt.Dimension(150, 50));
+        jTextFieldInstagram.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextFieldInstagramActionPerformed(evt);
+            }
+        });
+
+        jRadioButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jRadioButton1.isSelected()) {
+                    jLabel6.setVisible(true);
+                    jLabel6.setText("WhatsApp");
+                    jTextFieldWhatsApp.setVisible(true);
+                } else if(!jRadioButton1.isSelected()) {
+                    jLabel6.setVisible(false);
+                    jTextFieldWhatsApp.setVisible(false);
+                }
+            }
+        });
+
+        jRadioButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jRadioButton2.isSelected()) {
+                    jLabel6.setVisible(true);
+                    jLabel6.setText("Email");
+                    jTextFieldEmail.setVisible(true);
+                } else if(!jRadioButton2.isSelected()) {
+                    jLabel6.setVisible(false);
+                    jTextFieldEmail.setVisible(false);
+                }
+            }
+        });
+
+        jRadioButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jRadioButton3.isSelected()) {
+                    jLabel6.setVisible(true);
+                    jLabel6.setText("Instagram");
+                    jTextFieldInstagram.setVisible(true);
+                } else if(!jRadioButton3.isSelected()) {
+                    jLabel6.setVisible(false);
+                    jTextFieldInstagram.setVisible(false);
+                }
+            }
+        });
+
+        jButton1.setText("Adicionar");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jTextFieldWhatsApp, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jRadioButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jRadioButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jRadioButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(0, 108, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(140, 140, 140))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jRadioButton1)
+                    .addComponent(jRadioButton2)
+                    .addComponent(jRadioButton3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextFieldWhatsApp, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(0, 8, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>                        
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    }                                           
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    }                                           
+
+    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    }                                           
+
+    private void jTextFieldWhatsAppActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    } 
     
-    public void update() {
+    private void jTextFieldEmailActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    } 
 
+    private void jTextFieldInstagramActionPerformed(java.awt.event.ActionEvent evt) {                                            
+        // TODO add your handling code here:
+    } 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ContatoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ContatoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ContatoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ContatoWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ContatoWindow(null, null).setVisible(true);
+            }
+        });
     }
 
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JRadioButton jRadioButton3;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextFieldWhatsApp;    
+    private javax.swing.JTextField jTextFieldEmail;
+    private javax.swing.JTextField jTextFieldInstagram;
 
 
-    public void show() {
+    // End of variables declaration                   
+
+
+    
+    
+    /*public void show() {
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+
+        // Chame setVisible(true) após adicionar os componentes
         frame.setVisible(true);
-    }
+    } */
+
 }
 
